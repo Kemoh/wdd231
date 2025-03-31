d39ee3b2fe07bb1420597709f3a99a61bf4d5478
Kemoh <bayohumaru66@gmail.com>
2025-03-30 19:00:30 -0900

    Added to join.html

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,7 +1,37 @@
-// Get the DOM element for output:
+// Get the DOM element for the date timestamp
 document.addEventListener('DOMContentLoaded', function () {
     // Add timestamp on page load
-    document.getElementById('timestamp').value = new Date().toISOString();
+    document.getElementById('#timestamp').value = new Date().toISOString();
     console.log(timestamp);
 })
+
+// Get the DOM div element for showing the results of the form inputs
+const getString = window.location.search;
+console.log(getString);
+
+const myInfo = new URLSearchParams(window.location.search);
+//console.log(myInfo);
+
+// console.log(myInfo.get('firstName'));
+// console.log(myInfo.get('lastName'));
+// console.log(myInfo.get('email'));
+// console.log(myInfo.get('phone'));
+// console.log(myInfo.get('Organizational Title'));
+// console.log(myInfo.get('Business Name'));
+// console.log(myInfo.get('Business Description'));
+// console.log(myInfo.get('Membership Level'));
+
+
+document.querySelector("#result").innerHTML = `
+<p>Your first name is: ${myInfo.get('firstName')}<p/>
+<p>Your last name is: ${myInfo.get('lastName')}<p/>
+<p>Your email is: ${myInfo.get('email')}</p>
+<p>Your Phone: ${myInfo.get('phone')}</p>
+<p>Your organization's title is: ${myInfo.get('Organizational Title')} </P>
+<p>Your registered business/organizatio's name is: ${myInfo.get('Business Name')} </P>
+<p>Your brief business/organization's description is: ${myInfo.get('Business Description')} </P>
+<p>Your membership level selected is: ${myInfo.get('Membership Level')}</P>
+<p>The date and time that the form was uploaded is: ${myInfo.get('timestamp')}</P>
+`
+
 
---
+++ chamber/scripts/membership.js
@@ -0,0 +1,31 @@
+// Create a Membership level cards
+export const membership = [
+    {
+        levelName: "Non Profit Membership",
+        levelDescription: "This membership is specifically designed for non-profit organizations and comes at no cost.",
+        levelBenefits: "Focused on supporting non-profits with access to networks and events tailored for them.",
+        npLink: <a href="np-membership-details.html" target="_blank" class="np-membership-details">Learn More
+        </a> 
+    },
+    {
+        levelName: "Bronze Membership",
+        levelDescription: "This is a basic membership that gives users access to essential services at a low cost.",
+        levelBenefits: " Offers basic benefits such as event discounts, webinars, and networking opportunities.",
+        bronzeLink: <a href="bronze-membership-details.html" target="_blank" class="bronze-membership-details">Learn More
+        </a> 
+    },
+    {
+        levelName: "Silver Membership",
+        levelDescription: "This membership level offers more advanced benefits, perfect for growing organizations looking for enhanced support.",
+        levelBenefits: "More comprehensive features, including advanced training, priority event registration, and spotlight promotion.",
+        silverLink: <a href="silver-membership-details.html" target="_blank" class="silver-membership-details">Learn More
+        </a> 
+    },
+    {
+        levelName: "Gold Membership",
+        levelDescription: "The Gold Membership is the highest tier, offering all the benefits of the lower levels plus exclusive privileges, perfect for organizations that need top-tier services.",
+        levelBenefits: "The ultimate membership with exclusive VIP events, featured advertising, customized services, and direct access to experts.",
+        goldLink: <a href="np-membership-details.html" target="_blank" class="gold-membership-details">Learn More
+        </a> 
+    }
+]
