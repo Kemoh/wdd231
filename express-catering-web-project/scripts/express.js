543a740aed436f12009aa581cdf5382f94fa7a28
Umaru Bayoh <bayohumau66@gmail.com>
2025-04-12 12:21:58 +0100

    Review Express Project

--- express-catering-web-project/scripts/express.js
+++ express-catering-web-project/scripts/express.js
@@ -4,6 +4,6 @@ import { displayFooterInfo } from './bundle.js';
 // Call the function to display the footer information
 displayFooterInfo();
 
-
 import { dishes } from "../data/dishes.mjs";
 console.log(dishes);
+
