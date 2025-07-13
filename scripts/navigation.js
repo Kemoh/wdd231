bca6854731c11b656849584e14ace9f39f4f7ec7
Umaru Bayoh <bayohumau66@gmail.com>
2025-07-13 13:59:15 +0000

    Added companies' logo

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,7 +3,7 @@ const navButton = document.querySelector("#ham-btn");
 const navBar = document.querySelector("#nav-bar");
 
 // Toggle the navButton and the navBar on and off
-navButton.addEventListener('click', () => {
+navButton.addEventListener('click', () =>{
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
